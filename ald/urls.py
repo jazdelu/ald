c49ba0dd74a29583c6acf5c4b51a4fa7c2a12080
jazdelu <lushizhao@qq.com>
2016-06-01 23:23:09 +0800

    ..

--- ald/urls.py
+++ ald/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="comming.html")),

e2982c47248fb7b33a80fe53053bec03fe05ba83
jazdelu <lushizhao@qq.com>
2016-06-06 19:42:21 +0800

    .

--- ald/urls.py
+++ ald/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="comming.html")),
     url(r'^index/$', TemplateView.as_view(template_name="index.html")),
     url(r'^shoes/$', TemplateView.as_view(template_name="product.html")),
     url(r'^accessories/$', TemplateView.as_view(template_name="product.html")),

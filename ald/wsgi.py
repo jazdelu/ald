4cc907ec8e22d61af1a3e57719489e3ec989d9ea
jazdelu <lushizhao@qq.com>
2016-06-30 16:43:29 +0800

    .

--- ald/wsgi.py
+++ ald/wsgi.py
@@ -6,9 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ald.settings")
